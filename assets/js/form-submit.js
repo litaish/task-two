f8e70aa4244799c04e100222a220f34b1ae3b8fd
litaish <l.strode000@gmail.com>
2022-03-06 14:36:14 +0200

    Added success message to form submit event

---
+++ assets/js/form-submit.js
@@ -0,0 +1,33 @@
+const submitBtn = document.getElementById('contactSubmit');
+// Group of elements that will be removed and replaced by success message
+const successGroup = document.getElementById('successGroup');
+
+const successMsgLarge = document.createElement('p');
+successMsgLarge.innerText = 'Paldies, ka sapņo!';
+successMsgLarge.classList.add('f-mont-yellow-success-large');
+successMsgLarge.style.marginBottom = '2.125em';
+
+const successMsgsmall = document.createElement('p');
+successMsgsmall.innerText = 'Ja tavs sapnis tiks izvēlēts, mēs ar tevi sazināsimies.';
+successMsgsmall.classList.add('f-mont-yellow-success-small');
+
+
+const form = document.getElementById('contactForm');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Remove original form content
+    successGroup.innerHTML = '';
+    successGroup.style.textAlign = 'center';
+    // Replace with success message
+    successGroup.appendChild(successMsgLarge);
+    successGroup.appendChild(successMsgsmall);
+
+    // Clear form fields
+    form.reset();
+})
+
+
+
+
+
