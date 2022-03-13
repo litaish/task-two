42c0eaabee2e3f58ee2f02dcb17a6101a273730e
litaish <l.strode000@gmail.com>
2022-03-13 10:12:27 +0200

    Rewrote info retrieving functionality using callbacks

--- assets/js/map.js
+++ assets/js/map.js
@@ -24,22 +24,20 @@ function findIds() {
       if (status === google.maps.places.PlacesServiceStatus.OK && results) {
         for (let i = 0; i < results.length; i++) {
           // Add results to an array that contains all results of ID's
-
           allId.push(results[i].place_id);
-          // console.log(allId[i])
         }
       }
     });
   });
-  console.log(allId);
   return allId;
 }
 
-function findAdditionalInfo(requestedIds) {
-  requestedIds.forEach((Id) => {
+async function findAdditionalInfo(requestedIds, callback) {
+  var allInfo = [];
+
+  requestedIds.forEach(async (Id, i) => {
     // Collect information about places by place_id that is not accessible by regular query search
     let additionalRequest = {
-      // placeId: 'ChIJx-XDiMbP7kYRn7vptgFhk4Q',
       placeId: Id,
       fields: [
         "name",
@@ -47,53 +45,23 @@ function findAdditionalInfo(requestedIds) {
         "place_id",
         "geometry",
         "international_phone_number",
+        "type",
       ],
     };
 
-    // Get details from ID
-    service.getDetails(additionalRequest, (place, status) => {
-      // place object contains all returned fields. Recieve info by, for example, accessing place.place_id
-      if (
-        status === google.maps.places.PlacesServiceStatus.OK &&
-        place &&
-        place.geometry &&
-        place.geometry.location
-      ) {
-        const marker = new google.maps.Marker({
-          map,
-          position: place.geometry.location,
-        });
+    const { results, status } = await new Promise((resolve) =>
+      service.getDetails(
+        additionalRequest,
+        // pass a callback to getDetails that resolves the promise
+        (results, status) => resolve({ results, status })
+      )
+    );
 
-        // console.log(place.international_phone_number) // How to get info
-        // console.log(place.address_components[4].long_name) // How to get info
-
-        // On click on marker, display information recieved from the requests
-        google.maps.event.addListener(marker, "click", () => {
-          // Pop up window displaying all information about location on map
-          const content = document.createElement("div");
-          const nameElement = document.createElement("h2");
-
-          nameElement.textContent = place.name;
-          content.appendChild(nameElement);
-
-          const placeIdElement = document.createElement("p");
-
-          placeIdElement.textContent = place.place_id;
-          content.appendChild(placeIdElement);
-
-          const placeAddressElement = document.createElement("p");
-
-          placeAddressElement.textContent = place.address_components[2];
-          content.appendChild(placeAddressElement);
-          infowindow.setContent(content);
-          infowindow.open(map, marker);
-        });
-      }
-    });
+    callback(results, status, printEverythingIntheArr, filterSmth);
   });
 }
 
-function initMap() {
+async function initMap() {
   const riga = new google.maps.LatLng(56.949907201814675, 24.10355411105006);
 
   infowindow = new google.maps.InfoWindow();
@@ -102,12 +70,60 @@ function initMap() {
     zoom: 12,
   });
 
-  // Creates service
   service = new google.maps.places.PlacesService(map);
 
   let requestedIds = findIds();
 
-  setTimeout(() => {
-    findAdditionalInfo(requestedIds);
+  // Wait for ID array to populate and run find additional info function
+  setTimeout(async () => {
+    findAdditionalInfo(requestedIds, printTheArray);
   }, 1000);
+}
+
+var test = [];
+function printTheArray(place, status, callback, callback2) {
+  if (
+    status === google.maps.places.PlacesServiceStatus.OK &&
+    place &&
+    place.geometry &&
+    place.geometry.location
+  ) {
+    const marker = new google.maps.Marker({
+      map,
+      position: place.geometry.location,
+    });
+
+    // On click on marker, display information recieved from the requests
+    google.maps.event.addListener(marker, "click", () => {
+      // Pop up window displaying all information about location on map
+      const content = document.createElement("div");
+      const nameElement = document.createElement("h2");
+
+      nameElement.textContent = place.name;
+      content.appendChild(nameElement);
+
+      const placeIdElement = document.createElement("p");
+
+      placeIdElement.textContent = place.place_id;
+      content.appendChild(placeIdElement);
+
+      const placeAddressElement = document.createElement("p");
+
+      placeAddressElement.textContent = place.address_components[3].long_name;
+      content.appendChild(placeAddressElement);
+      infowindow.setContent(content);
+      infowindow.open(map, marker);
+    });
+  }
+
+  test.push([place, status]);
+  callback(test);
+  callback2(test);
+}
+
+function printEverythingIntheArr(arr) {
+  console.log(arr);
+}
+function filterSmth(arr) {
+  console.log(arr);
 }
