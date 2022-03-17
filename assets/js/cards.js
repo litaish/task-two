8befb17803f30980e4f893732e9214c1b3c2b267
litaish <l.strode000@gmail.com>
2022-03-17 15:51:48 +0200

    Fixed navbar, footer links, added story page and file cards.json

---
+++ assets/js/cards.js
@@ -0,0 +1,74 @@
+console.log("hello");
+
+const activeCards = [
+  {
+    id: 1,
+    video: false,
+  },
+  {
+    id: 2,
+    video: false,
+  },
+  {
+    id: 3,
+    video: false,
+  },
+  {
+    id: 4,
+    video: false,
+  },
+  {
+    id: 5,
+    video: "This is a video link",
+  },
+  {
+    id: 6,
+    video: false,
+  },
+  {
+    id: 7,
+    video: false,
+  },
+  {
+    id: 8,
+    video: false,
+  },
+  {
+    id: 9,
+    video: false,
+  },
+  {
+    id: 10,
+    video: false,
+  },
+  {
+    id: 11,
+    video: false,
+  },
+  {
+    id: 12,
+    video: false,
+  },
+  {
+    id: 13,
+    video: false,
+  },
+  {
+    id: 14,
+    video: false,
+  },
+  {
+    id: 15,
+    video: false,
+  },
+  {
+    id: 16,
+    video: false,
+  },
+  {
+    id: 17,
+    video: false,
+  },
+];
+
+
